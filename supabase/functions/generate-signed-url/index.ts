cf99cfdb615026aafebc9229ea4b5b770ea1831e
Alexandra <wardrobe.ai.application@gmail.com>
2025-12-02 19:08:29 +0200

    fix: Fix CORS header

--- supabase/functions/generate-signed-url/index.ts
+++ supabase/functions/generate-signed-url/index.ts
@@ -11,7 +11,7 @@ declare const Deno: {
 };
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'http://localhost:3000',
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
   'Access-Control-Allow-Credentials': 'true'
